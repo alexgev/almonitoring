39f13fe57e2641d1426fad091e7db321282b1635
alexgev <alexgev1997@gmail.com>
2018-08-07 03:54:15 +0300

    test

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -26,19 +26,19 @@ module.exports.bootstrap = function(cb) {
   bot = new TelegramBot(token, {polling: true});
   
   // Matches "/echo [whatever]"
-  bot.onText(/\/start/, (msg, match) => {
+  bot.onText(/\/start/, (msg) => {
     // 'msg' is the received Message from Telegram
     // 'match' is the result of executing the regexp above on the text content
     // of the message
-    chatId = msg.chat.id;
+    var chatId = msg.chat.id;
     // var resp = match[1]; // the captured "whatever"
   
     // send back the matched "whatever" to the chat
     bot.sendMessage(chatId, 'Enter password');
   });
   
-  bot.onText(new RegExp(variables.passFromTelegram), (msg) => {
-    chatId = msg.chat.id;
+  bot.onText(/qwer/, (msg) => {
+    var chatId = msg.chat.id;
     var message = 'You successfully authorized' + ((externalIp) ? ('\r\nYour external ip: ' + ip) : '')
     bot.sendMessage(chatId, message);
     chats.push(chatId);
